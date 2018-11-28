15b9f5357fb7096a6b67d78208ac4527d263c608
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2018-11-28 22:23:35 +0200

    simple fix for issue-5

--- docs_source/src/App.js
+++ docs_source/src/App.js
@@ -37,6 +37,7 @@ class App extends Component {
           />
 
           <Sider
+            width={250}
             className={classNames({
               sider: this.state.isMenuHide,
               "js-sider": !this.state.isMenuHide
