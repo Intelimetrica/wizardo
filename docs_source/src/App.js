3e8093e8eb1d7bb23094e773385ebd31792668ec
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2018-11-30 17:24:57 +0200

    Make navbar collapsable in the screen less than 885px [without classnames]

--- docs_source/src/App.js
+++ docs_source/src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link, Router, Switch, Route } from "react-static";
 import Routes from "react-static-routes";
 import { hot } from "react-hot-loader";
 import { Layout } from "antd";
 const { Sider, Content, Footer } = Layout;
+
 //;
 import Navbar from "./components/Navbar";
+import MenuIcon from "./components/MenuIcon";
 import Guides from "./containers/Guides";
 import logo_horizontal from "./images/logo_horizontal.svg";
 
@@ -15,38 +17,52 @@ import "../node_modules/highlight.js/styles/atom-one-dark.css";
 
 /* TODO: remove blog and request of blogs in compilation */
 
-const App = () => (
-  <Router>
-    <Layout>
-      <Sider
-        style={{
-          height: "100%",
-          overflowY: "scroll",
-          position: "fixed",
-          left: 0
-        }}
-        width={250}
-      >
-        <Link to="/">
-          <img className="logo" src={logo_horizontal} alt="logo" />
-        </Link>
-        <Navbar />
-      </Sider>
-      <Layout style={{ marginLeft: 250 }}>
-        <Content style={{ minHeight: "calc(100vh - 70px)" }}>
-          <Router>
-            <Switch>
-              <Route path="/guides" component={Guides} />
-              <Routes />
-            </Switch>
-          </Router>
-        </Content>
-        <Footer style={{ textAlign: "center" }}>
-          {`Wizardo ©${new Date().getFullYear()} Created by Intelimétrica`}
-        </Footer>
-      </Layout>
-    </Layout>
-  </Router>
-);
+class App extends Component {
+  state = {
+    isMenuHide: true
+  };
+
+  onClickIcon = () => {
+    this.setState({ isMenuHide: !this.state.isMenuHide });
+  };
+
+  render() {
+    const isMenuHide = this.state.isMenuHide;
+
+    return (
+      <Router>
+        <Layout>
+          <MenuIcon
+            isMenuHide={this.state.isMenuHide}
+            onClick={this.onClickIcon}
+          />
+
+          <Sider
+            width={250}
+            className={`${isMenuHide && "sider"} ${!isMenuHide && "js-sider"}`}
+          >
+            <Link to="/">
+              <img className="logo" src={logo_horizontal} alt="logo" />
+            </Link>
+            <Navbar />
+          </Sider>
+          <Layout className="layout">
+            <Content style={{ minHeight: "calc(100vh - 70px)" }}>
+              <Router>
+                <Switch>
+                  <Route path="/guides" component={Guides} />
+                  <Routes />
+                </Switch>
+              </Router>
+            </Content>
+            <Footer style={{ textAlign: "center" }}>
+              {`Wizardo ©${new Date().getFullYear()} Created by Intelimétrica`}
+            </Footer>
+          </Layout>
+        </Layout>
+      </Router>
+    );
+  }
+}
 
 export default hot(module)(App);
