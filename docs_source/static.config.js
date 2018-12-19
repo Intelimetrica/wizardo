7b6d93de58564344791dd0697dd6dc10a9aa6816
Ivan P. Burgos <ivan.pburgos@gmail.com>
2018-12-18 18:25:31 -0600

    replace single quotes by double quotest

--- docs_source/static.config.js
+++ docs_source/static.config.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default {
-  basePath: 'wizardo',
+  basePath: "wizardo",
   getSiteData: () => ({
-    siteTitle: 'Wizardo',
-    metaDescription: 'Wizardo is tool that allows you to create code generators to bootstrap new projects or new features in existing ones.',
-    favicon: 'https://s3-us-west-2.amazonaws.com/assets-general-im/wizardo/wizardo.ico',
+    siteTitle: "Wizardo",
+    metaDescription: "Wizardo is a tool that allows you to create code generators to bootstrap new projects or new features in existing ones.",
+    favicon: "https://s3-us-west-2.amazonaws.com/assets-general-im/wizardo/wizardo.ico",
     logo: "https://s3-us-west-2.amazonaws.com/assets-general-im/wizardo/logo.png",
-    urlWizardo: "https://intelimetrica.github.io/wizardo"
+    urlWizardo: "https://intelimetrica.github.io/wizardo",
   }),
   Document: class CustomHtml extends Component {
     render() {
@@ -75,23 +75,23 @@ export default {
     }
   },
   getRoutes: async () => {
-    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    const { data: posts } = await axios.get("https://jsonplaceholder.typicode.com/posts")
     return [
       {
-        path: '/',
-        component: 'src/containers/Home',
+        path: "/",
+        component: "src/containers/Home",
       },
       {
-        path: '/contribute',
-        component: 'src/containers/Contribute',
+        path: "/contribute",
+        component: "src/containers/Contribute",
       },
       {
-        path: '/guides',
-        component: 'src/containers/Guides',
+        path: "/guides",
+        component: "src/containers/Guides",
       },
       {
         is404: true,
-        component: 'src/containers/404',
+        component: "src/containers/404",
       },
     ]
   },
